cdc733faee09172322f14553c2a3d37b18fb0054
ccerneka <ccerneka29@setoncatholic.org>
2026-09-17 21:32:33 +0000

    completed the assignment

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name= input("what is your name?")
-print("hello",name)
+name=input("enter your name: ")
+age=input("enter your age: ")
+color=input("enter your color: ")
+print("Hell0,", name, "you are", age, "and your favorite color is", color)
